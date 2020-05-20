6730d5d4bb78417a18f3183fcca4abe54541881b
JEFT-hai <3433891733@qq.com>
2020-05-20 12:00:44 +0800

    babel

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,13 @@
+/*
+ * @Author: your name
+ * @Date: 2020-05-20 10:51:33
+ * @LastEditTime: 2020-05-20 12:00:18
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \jeft-vue-plugin\jeft-vue-growheight\babel.config.js
+ */ 
 module.exports = {
   presets: [
-    // '@vue/cli-plugin-babel/preset'
+    '@vue/cli-plugin-babel/preset'
   ]
 }
